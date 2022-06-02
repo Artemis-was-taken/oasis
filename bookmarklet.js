32c1f287849df8379e9cc87494eab0b3dcdf57f7
Artemis-was-taken <94702957+Artemis-was-taken@users.noreply.github.com>
2022-06-01 21:35:26 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,4 @@
-javascript:(function(){if(document.getElementById('__wc_display')){__wc_refresh();return} // That was the guard. The rest of the code will load the js files
- 
-window.__wc_base='http://charcount.appspot.com/';var%20d=document;var%20s=d.createElement('script');s.setAttribute('src',__wc_base+'s/jquery.js');s.setAttribute('type','text/javascript');d.body.appendChild(s);s=d.createElement('script');s.setAttribute('src',__wc_base+'s/wc.js');s.setAttribute('type','text/javascript');d.body.appendChild(s);})()
+javascript: (function(){var s = document.createElement('script'); // Create a script dom element
+s.setAttribute('src', 'http://charcount.appspot.com/s/jquery.js'); // Set the element source to the js file you want to load
+s.setAttribute('type', 'text/javascript');
+document.body.appendChild(s);}()
