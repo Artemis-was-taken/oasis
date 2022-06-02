ddb2e09bdf39ebc4d17d46d41d535eeaf3edf115
Artemis-was-taken <94702957+Artemis-was-taken@users.noreply.github.com>
2022-06-01 21:27:32 -0400

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,3 @@
+javascript:(function(){if(document.getElementById('__wc_display')){__wc_refresh();return} // That was the guard. The rest of the code will load the js files
+ 
+window.__wc_base='http://charcount.appspot.com/';var%20d=document;var%20s=d.createElement('script');s.setAttribute('src',__wc_base+'s/jquery.js');s.setAttribute('type','text/javascript');d.body.appendChild(s);s=d.createElement('script');s.setAttribute('src',__wc_base+'s/wc.js');s.setAttribute('type','text/javascript');d.body.appendChild(s);})()
