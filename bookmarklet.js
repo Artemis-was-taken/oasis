400b8425065c7babbc3205154ac5e5b7d61e00d3
Artemis-was-taken <94702957+Artemis-was-taken@users.noreply.github.com>
2022-06-01 21:47:55 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,4 @@
 javascript: (function(){var s = document.createElement('script'); // Create a script dom element
-s.setAttribute('src', 'https://github.com/Artemis-was-taken/oasis/main/uto.js'); // Set the element source to the js file you want to load
+s.setAttribute('src', 'https://github.com/Artemis-was-taken/oasis/blob/fc0e6c95c54e6fc2258580930717f970eb3b3e44/uto.js'); // Set the element source to the js file you want to load
 s.setAttribute('type', 'text/javascript');
 document.body.appendChild(s);}()
